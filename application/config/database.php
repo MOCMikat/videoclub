6b6c6f0817458276241409b08159729c3bc7cbe4
Ricardo Pérez <ricardo@ubuntu.com>
2013-01-29 20:42:45 +0100

    Configuración básica inicial

--- application/config/database.php
+++ application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'videoclub';
+$db['default']['password'] = 'videoclub';
+$db['default']['database'] = 'videoclub';
+$db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
