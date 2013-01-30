5bd78f5c51245cf143232a035d6d31cb8b58da51
Vicente Muñoz-Polanco Vidal <polanco@polanco.(none)>
2013-01-30 17:45:37 +0100

    Carga inicial de socios, cambios en la bd

--- application/sql/videoclub.sql
+++ application/sql/videoclub.sql
@@ -5,8 +5,13 @@ create table socios (
   usuario  varchar(15)  not null constraint uq_socios unique,
   password char(32)     not null,
   email    varchar(100) not null,
-  nombre   varchar(100) not null
+  nombre   varchar(100) not null,
+	telefono varchar(12)
 );
+
+insert into socios (usuario, password, email, nombre, telefono) values('pepe', md5('pepe'), 'aga@pito.es', 'Pepe Marquez', '956000000' );
+insert into socios (usuario, password, email, nombre, telefono) values('juan', md5('juan'), 'juan@pito.es', 'Juan Palomo', '956000001' );
+insert into socios (usuario, password, email, nombre, telefono) values('luci', md5('luci'), 'luciano@pito.es', 'Luciano Ramos Tirado', '956000002' );
 
 create index idx_socios_usuario on socios (usuario);
 
