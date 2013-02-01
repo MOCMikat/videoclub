290f2d23a82fdc00e2daf9d4adcd5e339cc149f3
Manuel Sánchez <manuel.sanchez@iesdonana.org>
2013-02-01 19:36:52 +0100

    Corrección errores estructuras y archivos socios

--- application/controllers/admin/socios.php
+++ application/controllers/admin/socios.php
@@ -1,15 +1,44 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Socios extends CI_Controller {
-	
-	function baja($id){
-
-		if ($baja) {
-			$this->Socio->baja($baja);
-		}
-		
-		$this->load->view->('socios/confirmar',$id);
-		
-	}  
+    
+    function __construct() {
+      parent::__construct();
+      $this->load->model('Socio');
+    }
+    
+    function index() {
+      
+      $columnas = array('usuario'   => 'Usuario',
+                        'email'     => 'Email',
+                        'nombre'    => 'Nombre',
+                        'telefono'  => 'Telñefono');
+                        
+      $res = $this->Socio->obtener_socios();
+                        
+      $data = array('columnas'  => $columnas,
+                    'res'    => $res);
+                        
+      $this->load->view('socios/index', $data);
+    }
+    
+    function modificar($id = null) {
+      /* if ($id == null) {
+        $this->session->set_flashdata('mensaje', 
+               '<strong style="color: red">No se ha elegido cliente</strong>');
+        redirect('clientes/index');
+        return;
+      } */
+        $socio = $this->Socio->obtener_socios('id = ?', array($id));
+        $data = array('socio' => $socio);
+        $this->load->view('socios/modificar', $data);
+    }
+    
+   	function baja($id){
+  		if ($baja) {
+  			$this->Socio->baja($baja);
+  		}
+  		$this->load->view->('socios/confirmar',$id);
+	  }
 
 }
